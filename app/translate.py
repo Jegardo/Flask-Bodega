11edd9e0c4da62cf4625a72cccaaf8ea9b8441e3
Jegardo <panteliskavv@gmail.com>
2021-05-06 17:55:42 +0200

    fixed translator

--- app/translate.py
+++ app/translate.py
@@ -11,12 +11,10 @@ def translate(text, source_language, dest_language):
     auth = {
         'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
         'Ocp-Apim-Subscription-Region': 'northeurope'}
-
     r = requests.post(
-        'https://api.cognitive.microsofttranslator.com/'
+        'https://api.cognitive.microsofttranslator.com'
         '/translate?api-version=3.0&from={}&to={}'.format(
             source_language, dest_language), headers=auth, json=[{'Text': text}])
-
     if r.status_code != 200:
-        return _('Error:the translation service failed.')
+        return _('Error: the translation service failed.')
     return r.json()[0]['translations'][0]['text']
