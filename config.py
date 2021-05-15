2a7b33ff369f51c33f2367aa5ad2565d2261865b
Jegardo <panteliskavv@gmail.com>
2021-05-15 19:52:23 +0200

    sepuku pls

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    if 'LOG_TO_STDOUT':
+        SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
